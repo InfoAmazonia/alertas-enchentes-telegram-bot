2589ae60917733a947a2fb0131e1a3afa0d56a5f
Jefferson Neves <jefferson.rpn@gmail.com>
2016-09-30 15:05:43 -0300

    Update bot messages and commands

--- src/main/java/org/telegram/Commands.java
+++ src/main/java/org/telegram/Commands.java
@@ -8,36 +8,8 @@ package org.telegram;
  */
 public class Commands {
     public static final String commandInitChar = "/";
-    /// Weather forecast command
-    public static final String WEATHERCOMMAND = commandInitChar + "weather";
-    /// Current wether command
-    public static final String CURRENTWEATHERCOMMAND = commandInitChar + "current";
-    /// Transifex iOS command
-    public static final String transifexiOSCommand = commandInitChar + "langios";
-    /// Transifex android command
-    public static final String transifexAndroidCommand = commandInitChar + "langdroid";
-    /// Transifex android command
-    public static final String transifexTDesktop = commandInitChar + "langdesk";
-    /// Transifex android command
-    public static final String transifexWebogram = commandInitChar + "langweb";
-    /// Transifex android command
-    public static final String transifexWP = commandInitChar + "langwp";
-    /// Transifex android command
-    public static final String transifexOSX = commandInitChar + "langosx";
-    /// Transifex android support command
-    public static final String transifexAndroidSupportCommand = commandInitChar + "langtestdroid";
-    /// Help command
-    public static final String help = commandInitChar + "help";
-    /// Upload command
-    public static final String uploadCommand = commandInitChar + "upload";
-    /// Start command
-    public static final String startCommand = commandInitChar + "start";
-    /// Cancel command
-    public static final String cancelCommand = commandInitChar + "cancel";
-    /// Delete command
-    public static final String deleteCommand = commandInitChar + "delete";
-    /// List command
-    public static final String listCommand = commandInitChar + "list";
-    /// Start directions command
-    public static final String startDirectionCommand = commandInitChar + "directions";
+    public static final String START = commandInitChar + "iniciar";
+    public static final String HELP = commandInitChar + "ajuda";
+    public static final String STATUSRIOBRANCO = commandInitChar + "riobranco";
+    public static final String STATUSPORTOVELHO = commandInitChar + "portovelho";
 }
